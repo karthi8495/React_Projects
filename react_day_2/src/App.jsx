7dfaab661743343e624583e15f72e52d4fdeb416
karthikeyan <karthikeyanmadhappan@gmail.com>
2025-08-07 16:57:48 +0530

    updated

--- react_day_2/src/App.jsx
+++ react_day_2/src/App.jsx
@@ -4,22 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  let firstname = 'Karthikeyan';
+  let mentor_name = 'Haripriya';
+
+  let modulename = "React";
 
 
   return (
     <>
      <div>
-      Phone Directory
-      </div>
-      <hr />
-     <button>Add</button>
-     <hr />
-     
-     <div>
-       <span>Name</span><br />
-       <span>Phone</span>
+      <h1>Hi,This is Study Group</h1>
+      <p def>I am {firstname} Learning MERN Stock Developer and my instructor name is {mentor_name},I am interested in learn {modulename}.</p>
      </div>
-
     </>
   )
 }
