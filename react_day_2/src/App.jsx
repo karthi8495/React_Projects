1b5488ff9f9801b5ab3a8b267eb22fb587535173
karthikeyan <karthikeyanmadhappan@gmail.com>
2025-08-07 15:18:41 +0530

    New

---
+++ react_day_2/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <div>
+      Phone Directory
+      </div>
+      <hr />
+     <button>Add</button>
+     <hr />
+     
+     <div>
+       <span>Name</span><br />
+       <span>Phone</span>
+     </div>
+
+    </>
+  )
+}
+
+export default App
