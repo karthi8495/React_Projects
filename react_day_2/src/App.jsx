35fd650b82942e9a2f4240ba482ba7101599825d
karthikeyan <karthikeyanmadhappan@gmail.com>
2025-08-08 11:27:34 +0530

    donew

--- react_day_2/src/App.jsx
+++ react_day_2/src/App.jsx
@@ -3,20 +3,58 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+// Componenets no 1
 function App() {
-  let firstname = 'Karthikeyan';
-  let mentor_name = 'Haripriya';
 
-  let modulename = "React";
-
-
+  const stories = [
+    {
+      id:1,
+      title:"React Js",
+      description:"Description",
+      url:"https://react.dev/",
+      author:"Jordan Walke",
+    },
+    {
+      id:2,
+      title:"Node JS",
+      description:"Description",
+      url:"https://nodejs.org/en",
+      author:"Ryan Dahl",
+    }];
   return (
     <>
-     <div>
-      <h1>Hi,This is Study Group</h1>
-      <p def>I am {firstname} Learning MERN Stock Developer and my instructor name is {mentor_name},I am interested in learn {modulename}.</p>
-     </div>
+      <List  list = {stories}/>
+    
     </>
+  )
+}
+
+// Componenets no 2
+function List(props) {
+
+  return (
+     <ul>
+      {props.list.map((item) => (
+        <li key={item.id}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          
+        </li>
+      ))}
+
+     </ul>
+    
+  )
+}
+
+// Componenets no 3
+function Item() {
+ 
+  return (
+    <div>Item</div>
   )
 }
 
