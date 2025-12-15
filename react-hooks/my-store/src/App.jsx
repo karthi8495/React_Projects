ffc9d69e8dca0a8376edcde0abdbcea25e514579
karthikeyan <karthikeyanmadhappan@gmail.com>
2025-12-15 12:19:46 +0530

    update

--- react-hooks/my-store/src/App.jsx
+++ react-hooks/my-store/src/App.jsx
@@ -5,6 +5,12 @@ import './App.css'
 import Navbar from './components/Navbar'
 import ProductList from './components/ProductList'
 import CartModal from './components/CartModal'
+import Home from './components/Home'
+import Village from './components/Village'
+import Products from './components/Products'
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+
+
 
 function App() {
   const [cartItems, setCartItems] = useState([]);
@@ -23,15 +29,37 @@ function App() {
   };
 
   return (
-    <div className='bg-gray-200 min-h-screen'>
-     <Navbar cartCount = {cartItems.length} openCart={openCart}/>
-     <ProductList cartItems = { cartItems} setCartItems={setCartItems}/>
-     {
-      isCartOpen && (
-        <CartModal cartItems={cartItems}  closeCart={closeCart} removeFromCart={removeFromCart}/>
-      )
-     }
-    </div>
+    <Router>
+      <nav className='p-4 bg-yellow-500 text-white'>
+        <ul className='flex space-x-4'>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/village">Village</Link></li>
+          <li><Link to="/products">Products</Link></li> 
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/village' element={<Village />}></Route>
+
+        <Route path='/products' element={
+          <div>
+             <Navbar cartCount = {cartItems.length} openCart={openCart}/>
+              <ProductList cartItems = { cartItems} setCartItems={setCartItems}/>
+                 {
+                    isCartOpen && (
+                     <CartModal cartItems={cartItems}  closeCart={closeCart} removeFromCart={removeFromCart}/>
+                 )
+               }
+            </div>}></Route>  
+
+      </Routes>
+
+    </Router>
+
+    // <div className='bg-gray-200 min-h-screen'>
+    
+    // </div>
   )
 }
 
---
+++ react-hooks/my-store/src/components/Village.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Village() {
+  return (
+    <div><h1>
+      I Love You Village</h1></div>
+  )
+}
